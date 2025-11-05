44a39ddeb84ebdddd4f4062071325b7e67c02a36
yashraj7781 <yashrajdhoni151@gmail.com>
2025-11-05 12:54:44 +0530

    reactivating again for the last poc  lassttt

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -37,9 +37,7 @@ const Home = () => {
           </p>
 
           <div className="mt-8 flex flex-wrap gap-10 justify-center md:justify-start">
-            {/* <span className="text-red-600 font-semibold text-xl">Access restricted. To continue this service, reach out to the admin: Pritish@techjar.in
-, info@techjar.in
-.</span> */}
+         
             <Link
               to="/outbound-call"
               className="bg-[#7B3AED] text-white font-semibold px-6 py-3 w-40 rounded-lg shadow-lg hover:bg-[#6C2AD9] transition-all duration-300"
